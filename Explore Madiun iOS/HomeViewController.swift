f7418cfee83596723af0b8646ddfafe80ab390a4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-27 16:11:36 +0700

    :pencil: setup main tab menu

--- Explore Madiun iOS/HomeViewController.swift
+++ Explore Madiun iOS/HomeViewController.swift
@@ -7,10 +7,26 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTabBar()
     }
     
+    private func setupTabBar() {
+        self.tabBar.backgroundColor = UIColor.white
+        self.tabBar.tintColor = UIColor(named: "Primary")
+        if let items = self.tabBar.items {
+            items[0].title = "List Place"
+            items[0].image = UIImage(systemName: "list.bullet.rectangle")
+            items[1].title = "Gallery"
+            items[1].image = UIImage(systemName: "photo.on.rectangle")
+            items[2].title = "Maps"
+            items[2].image = UIImage(systemName: "map")
+            items[3].title = "About"
+            items[3].image = UIImage(systemName: "info.circle.fill")
+        }
+    }
 }
