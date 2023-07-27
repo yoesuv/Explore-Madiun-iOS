80e7ef03a3a753afc478b419392a8c3ee467dd6e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-27 11:16:10 +0700

    :tada: initial commit

---
+++ Explore Madiun iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Explore Madiun iOS
+//
+//  Created by Yusuf Saifudin on 27/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
