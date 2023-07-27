a83c5c25b200342ab10b0a5393e84f4604b09dee
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-27 14:28:05 +0700

    :newspaper: add home view controller

---
+++ Explore Madiun iOS/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Explore Madiun iOS
+//
+//  Created by Yusuf Saifudin on 27/07/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
