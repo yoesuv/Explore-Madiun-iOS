0bdb2f2a8fe8954486bd3f3cd166146bb0e1e7a6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-28 09:25:06 +0700

    :newspaper: widget place cell

---
+++ Explore Madiun iOS/Widgets/PlaceCell.swift
@@ -0,0 +1,20 @@
+//
+//  PlaceCell.swift
+//  Explore Madiun iOS
+//
+//  Created by Yusuf Saifudin on 28/07/23.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
