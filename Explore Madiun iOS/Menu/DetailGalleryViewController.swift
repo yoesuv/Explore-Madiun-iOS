c7fc95839ae1fb4327f35e6b4e344f389d615326
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-31 16:51:45 +0700

    :art: UI detail gallery

--- Explore Madiun iOS/Menu/DetailGalleryViewController.swift
+++ Explore Madiun iOS/Menu/DetailGalleryViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class DetailGalleryViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewDetailGallery: UIImageView!
+    @IBOutlet weak var labelGalleryDescription: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Detail Gallery"
+        
+        let imgUrl = URL(string: "https://lh3.googleusercontent.com/-VdGBzQkMOZI/VoHY6IZRcTI/AAAAAAAAB6I/V68FY5RlsgQ/s640-Ic42/alun_alun_madiun.jpg")
+        self.imageViewDetailGallery.kf.setImage(with: imgUrl)
+        self.labelGalleryDescription.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     }
 
 }
