1f855cb291241510d925342edb57bb2824eb2faf
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-01 10:54:36 +0700

    :pencil: load app version

--- Explore Madiun iOS/Menu/AboutInformationViewController.swift
+++ Explore Madiun iOS/Menu/AboutInformationViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 class AboutInformationViewController: UIViewController {
     
     var pageIndex: Int!
-
+    
+    @IBOutlet weak var imageViewInformation: UIImageView!
+    @IBOutlet weak var labelAppName: UILabel!
+    @IBOutlet weak var labelAppVersion: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.imageViewInformation.image = UIImage(named: "AppIcon60x60")
+        self.labelAppName.text = "Explore Madiun"
+        setupVersion()
+    }
+    
+    private func setupVersion() {
+        if let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") {
+            self.labelAppVersion.text = "Version \(appVersion)"
+        }
     }
 
 }
