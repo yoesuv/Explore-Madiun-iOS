e7db43b9027804e5469f108331705b65be80eab0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-01 14:23:45 +0700

    :art: content information

--- Explore Madiun iOS/Menu/AboutInformationViewController.swift
+++ Explore Madiun iOS/Menu/AboutInformationViewController.swift
@@ -14,6 +14,11 @@ class AboutInformationViewController: UIViewController {
     @IBOutlet weak var imageViewInformation: UIImageView!
     @IBOutlet weak var labelAppName: UILabel!
     @IBOutlet weak var labelAppVersion: UILabel!
+    @IBOutlet weak var imageViewEmail: UIImageView!
+    @IBOutlet weak var imageViewSocial: UIImageView!
+    @IBOutlet weak var imageViewSupport: UIImageView!
+    @IBOutlet weak var imageViewPolicy: UIImageView!
+    @IBOutlet weak var imageViewRepo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +26,17 @@ class AboutInformationViewController: UIViewController {
         self.imageViewInformation.image = UIImage(named: "AppIcon60x60")
         self.labelAppName.text = "Explore Madiun"
         setupVersion()
+        
+        self.imageViewEmail.image = UIImage(systemName: "envelope")
+        self.imageViewEmail.tintColor = .black
+        self.imageViewSocial.image = UIImage(systemName: "xmark.circle")
+        self.imageViewSocial.tintColor = .black
+        self.imageViewSupport.image = UIImage(systemName: "heart.fill")
+        self.imageViewSupport.tintColor = .systemPink
+        self.imageViewPolicy.image = UIImage(systemName: "info.square")
+        self.imageViewPolicy.tintColor = .black
+        self.imageViewRepo.image = UIImage(systemName: "folder")
+        self.imageViewRepo.tintColor = .black
     }
     
     private func setupVersion() {
