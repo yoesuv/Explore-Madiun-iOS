e6b17b3b075955bb9d57b2f0acf3259c32661fd7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-08-17 09:46:58 +0700

    :sparkles: maps location pin

--- Explore Madiun iOS/Menu/MapsViewController.swift
+++ Explore Madiun iOS/Menu/MapsViewController.swift
@@ -11,6 +11,8 @@ import GoogleMaps
 class MapsViewController: UIViewController, GMSMapViewDelegate {
     
     private var mapView: GMSMapView!
+    private let service = NetworkService()
+    private var pins:[PinModel] = []
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.navigationItem.title = "Maps"
@@ -18,13 +20,38 @@ class MapsViewController: UIViewController, GMSMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        requestData()
     }
     
     override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: -7.720012, longitude: 111.449379, zoom: 9)
-        mapView = GMSMapView(frame: .zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: -7.689211, longitude: 111.345548, zoom: 9)
+        self.mapView = GMSMapView(frame: .zero, camera: camera)
+        self.mapView.settings.compassButton = true
+        self.mapView.delegate = self
         self.view = mapView
-        mapView.delegate = self
+    }
+    
+    
+    private func requestData() {
+        self.mapView.clear()
+        service.fetchPins(result: {response in
+            self.pins.removeAll()
+            if response.error == nil {
+                self.pins = response.value ?? []
+                self.showMarkers()
+            }
+        })
+    }
+    
+    private func showMarkers() {
+        for pin in pins {
+            let marker = GMSMarker()
+            let locationLatLng = CLLocationCoordinate2D(latitude: pin.latitude ?? 0, longitude: pin.longitude ?? 0)
+            marker.icon = GMSMarker.markerImage(with: UIColor(named: "Primary")!)
+            marker.position = locationLatLng
+            marker.title = pin.name
+            marker.map = self.mapView
+        }
     }
 
 }
